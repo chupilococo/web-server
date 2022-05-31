afcf877d7e5b79571aeaa442f8fad4cf97beb7d0
chupilococo <danilo.carena@davinci.edu.ar>
2022-05-31 15:51:02 -0300

    setup for heroku

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,6 +4,7 @@ const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
 
     request({ url, json: true }, (error, { body }) => {
+        console.log(body)
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
